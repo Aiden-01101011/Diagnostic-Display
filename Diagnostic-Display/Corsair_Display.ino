57c86e07d04c692844e1c14ca53978506db06725
Aiden-01101011 <72362519+Aiden-01101011@users.noreply.github.com>
2022-03-29 19:32:36 -0400

    Update Corsair_Display.ino

--- Diagnostic-Display/Corsair_Display.ino
+++ Diagnostic-Display/Corsair_Display.ino
@@ -17,7 +17,7 @@ char buf[20];
 
 void setup() {
   Serial.begin(115200);
-  Serial.setTimeout(3);
+  Serial.setTimeout(1);
 
   uint16_t ID = tft.readID();
   if (ID == 0xD3D3) ID = 0x9481; //force ID if write-only display
@@ -34,11 +34,11 @@ void loop() {
   if (Serial.available() > 0){
     coolantTemp = Serial.readStringUntil('\n').toFloat();
     dtostrf(coolantTemp, -2, 2, buff);
-    Serial.print(x);
+    Serial.print(coolantTemp + String("\n"));
     tft.setCursor(0, 0);
     sprintf(buf, "Temp: %03s ", buff);
     tft.fillRect(0,0, 400, 100, BLACK);
     tft.print(buf);
-    delay(50);
+    delay(3000);
   }
 }
